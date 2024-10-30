23e668654749c880a7c8e488be63957acc9aeb38
jiangxin <2396694033@qq.com>
2024-10-30 22:35:49 +0800

    代码学习

---
+++ 代码学习/Python/numpy_run.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+print("Hello, Numpy!")
+print("Numpy version: ", np.__version__)
+
+a = np.array([1, 2, 3])
+b = np.random.rand(3)
+
+# Print the arrays
+
+print("a: ", a)
+print("b: ", b)
+
+# 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
