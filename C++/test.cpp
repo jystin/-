f90a929b22fe07723e4d33298403fac7002cc06b
xinjiang <2396694033@qq.com>
2024-10-31 21:56:32 +0800

    update

---
+++ C++/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <cctype>
+using namespace std;
+const int Foot2inch = 12;
+
+struct Person {
+    string name;
+    double height;
+    double weight;
+    int age = 2; // 有默认值的放在最后
+    union
+    {
+        long long_id = 0;
+        // string short_id = "000";
+    };
+};
+
+enum color{red, green, blue};
+
+typedef char APM;
+
+
+int main() {
+    int a;
+    while (cin >> a) {
+        cout << a << endl;
+    }
+    cout << a << endl;
+
+}
---
+++ Python/numpy/learn.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Create a 2D array 
+str = "jhuisfusu \n jkisjdfisj"
+
+print(str)
+
+res = str.splitlines()
+print(res)
